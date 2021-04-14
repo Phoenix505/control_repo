a94907f0b7144418c7d1e008abaed856bba8c301
Phoenix505 <82365415+Phoenix505@users.noreply.github.com>
2021-04-14 15:41:09 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,11 @@ node default {
 node '3756787f861c.mylabserver.com' {
   include role::master_server
 }
+node  /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
+}
   
