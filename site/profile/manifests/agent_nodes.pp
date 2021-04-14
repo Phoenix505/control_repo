4580014cd3f5747a1cce326ceda602c5fac62f5d
Phoenix505 <82365415+Phoenix505@users.noreply.github.com>
2021-04-14 15:08:43 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,6 @@
 class profile::agent_nodes {
   include dockeragernt
   dockeragent::node {'web.3756787f861c.mylabserver.com':}
-  dockeragent::node {'db.3756787f861c,mylabserver.com':]
+  dockeragent::node {'db.3756787f861c,mylabserver.com':}
   
   
