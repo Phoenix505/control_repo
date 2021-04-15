527fa28c21bcf84dfc65e53d3b22ce6d49bb6bb2
Phoenix505 <82365415+Phoenix505@users.noreply.github.com>
2021-04-15 10:22:47 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,5 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.3756787f861c.mylabserver.com':}
   dockeragent::node {'db.3756787f861c.mylabserver.com':}
+  dockeragent::node {'web.puppet.docker':}
+  dockeragent::node {'db.puppet.docker':}
 }
   
